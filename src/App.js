2523e6e7653a4c63b9082d6e58635de6542d183e
Dian Prasetyo <69192355+dianprsty@users.noreply.github.com>
2020-10-11 22:03:10 +0700

    hrd

--- src/App.js
+++ src/App.js
@@ -1,60 +1,18 @@
-import React, { Component } from 'react';
-import Home from './home/home';
-import ParkIn from './park-in/park-in';
-import './nav.css';
+import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom"
+import { Header, NavBar, Footer } from "./template"
+import './App.css';
 
-import { BrowserRouter as Router,Route,Link } from "react-router-dom";
-
-class App extends Component {
-
-
-  render() { 
-    return (
+function App() {
+  return (
+    <div className="App next">
       <Router>
-        <ul className="navbar navbar-expand-lg navbar-light bg-light">
-          <li>
-            <Link className="nav-link" to="/parkin">Masuk Parkir</Link>
-          </li>
-          <li>
-            <Link className="nav-link" to="/home">Manage Parkir</Link>
-          </li>
-        </ul>
-        
-        
-        
-        <Route path="/parkin" component={ParkIn}/>
-        <Route path="/home" component={Home}/>
+        <Header />
+        <NavBar />
+        <Footer />
       </Router>
-
-    )
-  }
+    </div>
+  );
 }
- 
+
 export default App;
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
